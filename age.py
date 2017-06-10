ba9fde81a070a72c711ff59a0838ef160e56b5ea
Tony Hairston <tony9692000@gmail.com>
2017-06-10 12:02:35 -0400

    Initial Commit

---
+++ age.py
@@ -0,0 +1,9 @@
+x = 2017
+
+name = input('What is you name? ')
+place = input('where were you born? ')
+year = input('What year where you born? ')
+print(name)
+print(place)
+print(int(x) - int(year))
+
---
+++ place.py
@@ -0,0 +1,12 @@
+x = 2017
+
+name = input('What is you name? ')
+place = input('where were you born? ')
+year = input('What year where you born? ')
+Military = input('how long were you in the Military? ')
+print('Hi ',name)
+print('That is a nice city', place)
+print('You are old',int(x) - int(year))
+print(Military,'Thank you for your service')
+
+print("Tony thank you for your {} years of service".format(Military))
